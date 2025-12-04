f97fc9739fc658162668d35a6d27f9cdca61bc7c
JPSieg <jpsieg225@gmail.com>
2025-12-03 21:20:40 -0500

    Fix extiction coefficient calculation and improve readability of the function

--- R/Calc_exctinction_coef.R
+++ R/Calc_exctinction_coef.R
@@ -16,38 +16,59 @@
 #' @export
 calc.extcoeff = function(NucAcid, wavelength = 260, silent = F){
   tryCatch({
-
-    df.ext = subset(df.ext.data, Wavelength.nm == wavelength)
-    RNA = df.ext$Absorbtivity.M
-    names(RNA) = df.ext$Nucleotide
-    b <- c()
-    b <- strsplit(NucAcid[-which(NucAcid == NucAcid[1])], split = "")
-    c <- {}
-    d <- {}
-    e <- c()
-    for (i in c(1:length(b))){
-      c[[i]] <- c(1:(length(b[[i]])-1))
-      d[[i]] <- c(2:(length(b[[i]])))
-      for (j in c(1:(length(b[[i]])-1))){
-        c[[i]][j] <- RNA[[which(names(RNA) == paste(b[[i]][j], "p", b[[i]][j+1], sep = ""))]]
+    df.ext = subset(df.ext.data, Wavelength.nm == wavelength) #Grab data for each wavelength
+    v_Absorbtivity = df.ext$Absorbtivity.M
+    names(v_Absorbtivity) = df.ext$Nucleotide
+    l_v_NucAcid = strsplit(NucAcid[-which(NucAcid == NucAcid[1])], split = "") #Makes a list of vectors containing mononucleotides from strings supplied in NucAcid
+    Ext = c() #Empty vector of extinction coefficients
+    for (i in c(1:length(l_v_NucAcid))){
+      #Check strand
+      if (silent){}else{
+        print("Sequence:")
+        print(NucAcid[i+1])
       }
-      for (j in c(2:(length(b[[i]])))){
-        d[[i]][j] <- RNA[[which(names(RNA) == paste(b[[i]][j], "p", sep = ""))]]
+      v_DiNuc = c() #Empty vector of dinucleotides
+      v_monoNuc = c() #Empty vector of internal mononucleotides
+      v_eDiNuc = c() #Empty vector of dinucleotides
+      v_emonoNuc = c() #Empty vector of internal mononucleotides
+      for (j in c(1:(length(l_v_NucAcid[[i]])-1))){ #Make a vector of dinucleotides
+        DiNuc = paste(l_v_NucAcid[[i]][j], "p", l_v_NucAcid[[i]][(j+1)], sep = "")
+        v_eDiNuc = c(v_eDiNuc, DiNuc)
+        v_DiNuc = c(v_DiNuc, v_Absorbtivity[[which(names(v_Absorbtivity) == DiNuc)]])
       }
-      e[i] <- 2*sum(c[[i]]) - sum(d[[i]])
+      #Check DiNuc
+      if (silent){}else{
+        print("Dinucleotides:")
+        print(v_eDiNuc)
+        print(v_DiNuc)
+      }
+      for (j in c(2:(length(l_v_NucAcid[[i]])-1))){ #Make a vector of mononucleotides (not including ends)
+        MonoNuc = paste(l_v_NucAcid[[i]][j], "p", sep = "")
+        v_emonoNuc = c(v_emonoNuc, MonoNuc)
+        v_monoNuc = c(v_monoNuc, v_Absorbtivity[[which(names(v_Absorbtivity) == MonoNuc)]])
+      }
+      #Check monoNuc
+      if (silent){}else{
+        print("Mononucleotides:")
+        print(v_emonoNuc)
+        print(v_monoNuc)
+      }
+      #Subract internal mononucleotides from double the external mononucleotides
+      Ext[i] = 2*sum(v_DiNuc) - sum(v_monoNuc)
     }
-    extcoef <- list()
-    extcoef[[1]] <- sum(e)
-    for (i in c(1:length(b))){
-      extcoef[[i+1]] <- e[i]
+    #Clean up and format a list of extinction coefficients
+    extcoef = list()
+    extcoef[[1]] = sum(Ext)
+    for (i in c(1:length(Ext))){
+      extcoef[[i+1]] = Ext[i]
     }
     names(extcoef) <- c("Total", NucAcid[c(2:length(NucAcid))])
+    if (silent){}else{
+      print("Final:")
+      print(extcoef)
+    }
+    return(extcoef)
   },
   error = function(e){print("There is no nucleotide extinction coefficient at this wavelength for a nucleotide you specified in your sequence. You will need to provide your own custom extinction coefficient")})
-  output = extcoef
-
-  if (silent){}else{
-    print(output)
-  }
-  output <- output
 }
+
